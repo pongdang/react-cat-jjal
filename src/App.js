77b113bcabe9794e7f5a1350a6c16b7693849902
pongdang <pongdang0121@gmail.com>
2022-02-03 17:20:04 +0900

    필요없는 코드 제거

--- src/App.js
+++ src/App.js
@@ -24,8 +24,6 @@ const fetchCat = async (text) => {
 
 function App() {
   const CAT1 = "https://cataas.com/cat/60b73094e04e18001194a309/says/react";
-  // const CAT2 = "https://cataas.com//cat/5e9970351b7a400011744233/says/inflearn";
-  // const CAT3 = "https://cataas.com/cat/595f280b557291a9750ebf65/says/JavaScript";
 
   const [counter, setCounter] = useState(() => {
     return jsonLocalStorage.getItem("counter");
